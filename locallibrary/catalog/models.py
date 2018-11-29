3457f7b29bd6b7064192bafc9a3c8155f4b70037
Ben Jung <jbehips@kaist.ac.kr>
2018-11-30 04:48:42 +0900

    sssssss

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -7,10 +7,10 @@ from django.urls import reverse
 
 class Evidence(models.Model):
     """Model representing a Crime evidence(not specific copy of evidence)."""
-    evi_case = models.ForeignKey('Evi_case', on_delete=models.SET_NULL, null=True)
+    evi_case = models.ForeignKey('Evi_case', on_delete=models.CASCADE, blank=True)
     evi_number = models.AutoField(primary_key=True)
-    evi_type = models.ForeignKey('Evi_type', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, null=True, help_text = 'Enter brief description of evidence')
+    evi_type = models.ForeignKey('Evi_type', on_delete=models.CASCADE, blank=True)
+    summary = models.TextField(max_length=1000, blank=True, help_text = 'Enter brief description of evidence')
     evi_time = models.DateTimeField(null=True, blank=True)
     signiture = models.ImageField(blank=True, null=True,)
     picture = models.ImageField(blank=True, null=True,
@@ -29,7 +29,7 @@ class Evidence(models.Model):
 
 class Evi_type(models.Model):
     """Model representing evidence type."""
-    name = models.CharField(max_length=200, primary_key=True, help_text = 'Enter a evidence type(e.g. fingerprint)')
+    name = models.CharField(max_length=200, primary_key=True, unique=True, help_text = 'Enter a evidence type(e.g. fingerprint)')
 
     class Meta:
         ordering = ['name']
